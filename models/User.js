2fd87982011e99685424004af181bad09522dbc1
nanou-11 <anais.jouaret@gmail.com>
2020-07-29 11:37:55 +0200

    addbcrypt

--- models/User.js
+++ models/User.js
@@ -1,37 +1,61 @@
 const Sequelize = require("sequelize");
 
+const bcrypt = require("bcrypt");
+
 const sequelizeInstance = require("../sequelize");
 
-const User = sequelizeInstance.define("User", {
-  id: {
-    type: Sequelize.UUID,
-    allowNull: false,
-    primaryKey: true,
-    defaultValue: Sequelize.UUIDV4,
+const User = sequelizeInstance.define(
+  "User",
+  {
+    id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    lastName: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },
+    firstName: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    github: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    linkedin: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
   },
-  lastName: {
-    type: Sequelize.STRING(100),
-    allowNull: false,
-  },
-  firstName: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-  },
-  password: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-  },
-  github: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-  },
-  linkedin: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      },
+      beforeUpdate: (user) => {
+        if (user.changed("password")) {
+          const salt = bcrypt.genSaltSync();
+          user.password = bcrypt.hashSync(user.get("password"), salt);
+        }
+      },
+    },
+  }
+);
+
+User.prototype.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
 module.exports = User;
